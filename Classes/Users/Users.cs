3377e046559a3039f57b5313e4870129c841313f
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-11 19:12:41 +0630

    pj setup

---
+++ Classes/Users/Users.cs
@@ -0,0 +1,33 @@
+﻿namespace asp.net_tuto_02.Classes.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public User() { }
+
+        public User(int id,string name ,string email,string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+
+    static class UserRepository
+    {
+        private static List<User> _users = new List<User>()
+        {
+            new User(1,"aung ","aung@123","123123123"),
+            new User(2,"susu","susu@123","123123123"),
+        };
+
+        public static List<User>? GetAllUsers() => _users.Count >0 ? _users: null ;
+
+        public static void AddUser(User? user) =>  _users.Add(user!);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using asp.net_tuto_02.Classes.Users;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    if(context.Request.Path == "/users")
+    {
+        // all users GET => /users
+        if(context.Request.Method == "GET")
+        {
+            List<User>? users = UserRepository.GetAllUsers();
+
+            if(users is null || users.Count() == 0)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsJsonAsync(new { message = "user not found!" });
+                return;
+            }
+
+            context.Response.StatusCode = 200;
+            await context.Response.WriteAsJsonAsync(users);
+            return;
+        }
+
+        // create users POST => /user
+        if (context.Request.Method == "POST")
+        {
+            using var reader = new StreamReader(context.Request.Body);
+            var body = await reader.ReadToEndAsync();
+            User? user = JsonSerializer.Deserialize<User>(body);
+
+            if (user is null) return;
+
+            UserRepository.AddUser(user);
+
+            context.Response.StatusCode = 201;
+            await context.Response.WriteAsJsonAsync(user);
+            return;
+        }
+    }
+});
+
+app.Run();
